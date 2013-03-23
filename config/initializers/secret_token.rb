3a6f670a9b2a629b87a0e3133659d7ef7672a9d2
aranagi <0@aranagi.com>
2013-03-23 16:09:14 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsterakoya::Application.config.secret_token = '72b164632f26de237ae9b150a77537deb5292ba3fd2874df92a81a70d5dc007c9474e998865a1d6cba53460f4530d09d0f3fc4cfead4517ceb124a4b81a17f00'
